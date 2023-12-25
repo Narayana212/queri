7fab23171e1826811b267a0502226cd73db37cfd
Raavi Narayana <114774469+Narayana212@users.noreply.github.com>
2023-12-25 10:53:14 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,10 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack:(config,{buildId,dev,defaultLoaders,webpack,isServer})=>{
-        config.resolve.alias.canvas=false
-        config.resolve.alias.encoding=false
-        return config
-    }
-}
-
-module.exports = nextConfig
+    async redirects() {
+      return [
+        {
+          source: '/sign-in',
+          destination: '/api/auth/login',
+          permanent: true,
+        },
+        {
+          source: '/sign-up',
+          destination: '/api/auth/register',
+          permanent: true,
+        },
+      ]
+    },
+    images: {
+        domains: ['lh3.googleusercontent.com'],
+      },
+  
+    webpack: (
+      config,
+      { buildId, dev, isServer, defaultLoaders, webpack }
+    ) => {
+      config.resolve.alias.canvas = false
+      config.resolve.alias.encoding = false
+      return config
+    },
+  }
+  
+  module.exports = nextConfig
